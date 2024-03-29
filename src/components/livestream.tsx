e44283ab25abfe9ca226aaadc42b5165cd06f043
Aaron Nwabuoku <aaron@chatkitty.com>
2024-03-29 13:15:14 -0400

    Add loading state

--- src/components/livestream.tsx
+++ src/components/livestream.tsx
@@ -1,24 +1,33 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {loadChatUi} from 'chatkitty';
 
 const Livestream = () => {
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const container = document.getElementById("chat-ui");
 
         if (container && container.hasChildNodes()) return // Chat UI has already been loaded
 
-       loadChatUi({
-            widgetId: 'TAXqsiNOIYIhfadh',
-            username: 'jane@user',
+        loadChatUi({
+            widgetId: 'TAXqsiNOIYIhfadh', // substituted staging project widget ID
+            username: 'test@user', // user created in guest mode
             container: {
                 height: '100%',
             },
-            route: {
+            route: { // route to event channel
                 name: 'event',
                 event: '41267743-rick-astley-at-sse-arena-belfast'
+            },
+            components: {
+                chat: (context) => ({
+                    onMounted: () => {
+                        setLoading(false)
+                    }
+                })
             }
         }, {
-            $environment: {
+            $environment: { // configure chatkitty SDK to target staging servers
                 mode: 'sandbox',
                 origin: 'https://ui.staging.chatkitty.com',
                 environment: 'staging'
